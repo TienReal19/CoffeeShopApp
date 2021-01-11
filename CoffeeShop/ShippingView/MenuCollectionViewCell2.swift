cae0276e13eba8aad93aaa4db0e6f50a888d8eea
Valerian1999 <bossysleeper99@gmail.com>
2021-01-12 00:10:47 +0700

    dynamic uicollectionview height

---
+++ CoffeeShop/ShippingView/MenuCollectionViewCell2.swift
@@ -0,0 +1,12 @@
+//
+//  MenuCollectionViewCell2.swift
+//  CoffeeShop
+//
+//  Created by Valerian   on 11/01/2021.
+//
+
+import UIKit
+
+class MenuCollectionViewCell2: UICollectionViewCell {
+    
+}
